a62699de25ee788d7bc69c96a9f6856d19638572
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-04-07 23:14:41 +0530

    Added

--- Gemini_2.0_Clone/src/Components/Main/Main.jsx
+++ Gemini_2.0_Clone/src/Components/Main/Main.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import './Main.css';
 import { assets } from "../../assets/assets";
+import {Context} from'../../context/Context'
+
 const Main = () => {
+    const{onSent,recentPrompt,showResult,loading,resultData,setInput,input}=useContext(Context);
     return (
         <div className="main">
             <div className="nav">
@@ -9,6 +12,10 @@ const Main = () => {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
+            {!showResult?
+            <>
+                
+           
                 <div className="greet">
                     <p><span>Hello, Dev.</span></p>
                     <p>How can I help you today?</p>
@@ -31,13 +38,22 @@ const Main = () => {
                         <img src={assets.code_icon} alt="" />
                     </div>
                 </div>
+                </>:<div className="result">
+                    <div className="result-title">
+                    <img src={assets.user_icon} alt=""/>
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon}/>
+                </div>
+                </div>}
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder="Enter a prompt"/>
+                        <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt"/>
                         <div>
                             <img src={assets.gallery_icon} />
                             <img src={assets.mic_icon} />
-                            <img src={assets.send_icon} />
+                            <img onClick={()=>onSent}src={assets.send_icon} />
                         </div>
                       
                     </div>
--- Gemini_2.0_Clone/src/config/gemini.js
+++ Gemini_2.0_Clone/src/config/gemini.js
@@ -1,13 +1,14 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: "" });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyChSQLdfDXpglkagY_bBnvkDqhYYTAai-s"});
 
-async function main() {
+async function main(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Explain how AI works in a few words",
+    contents: "Explain how AI works",
   });
   console.log(response.text);
+  return response.text();
 }
 
-main();
+export default main;
